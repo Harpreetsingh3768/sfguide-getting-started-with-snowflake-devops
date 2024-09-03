c20a87daf712306f4ce1e98082b7e9a9caa7c414
Harpreetsingh3768 <harpreet3768@gmail.com>
2024-09-02 20:45:47 -0400

    Update 01_setup_snowflake.sql

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -6,7 +6,7 @@ CREATE WAREHOUSE IF NOT EXISTS QUICKSTART_WH WAREHOUSE_SIZE = XSMALL, AUTO_SUSPE
 -- Separate database for git repository
 CREATE DATABASE IF NOT EXISTS QUICKSTART_COMMON;
 
-
+/*
 -- API integration is needed for GitHub integration
 CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
@@ -18,7 +18,26 @@ CREATE OR REPLACE API INTEGRATION git_api_integration
 CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
   API_INTEGRATION = git_api_integration
   ORIGIN = '<insert URL of forked GitHub repo>'; -- INSERT URL OF FORKED REPO HERE
+*/
 
+ CREATE OR REPLACE SECRET git_secret_token
+  TYPE = password
+  USERNAME = 'harpreetsingh3768'
+  PASSWORD = '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';
+
+CREATE OR REPLACE API INTEGRATION git_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/Harpreetsingh3768')
+  ALLOWED_AUTHENTICATION_SECRETS = (git_secret_token)
+  ENABLED = TRUE;
+
+ 
+
+CREATE OR REPLACE GIT REPOSITORY sfgithub_demo
+  API_INTEGRATION = git_api_integration
+  GIT_CREDENTIALS = git_secret_token
+  ORIGIN = 'https://github.com/Harpreetsingh3768/sfguide-getting-started-with-snowflake-devops.git';
+ 
 
 CREATE OR REPLACE DATABASE QUICKSTART_PROD;
 
